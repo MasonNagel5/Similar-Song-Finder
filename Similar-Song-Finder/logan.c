ff281ef81665cf71cf316368549a2c4bf13fae7e
Mason <masonanagel@gmail.com>
2025-02-15 19:28:58 -0800

    Updated scoring from + 2 to + 1

--- Similar-Song-Finder/logan.c
+++ Similar-Song-Finder/logan.c
@@ -30,22 +30,22 @@ void traverse_through_available(Node** pHead) {
 void score_record(Node *pHead, Node *chosen_song, Preferences preference) {
 	int score = 0;
 
-	if (strcmp(chosen_song->data.artist, pHead->data.artist) == 0) score += (preference.artist) ? 2 : 1;
+	if (strcmp(chosen_song->data.artist, pHead->data.artist) == 0) score += (preference.artist) ? 1 : 0;
 
-	if(strcmp(chosen_song->data.genre[0], pHead->data.genre[0])) score += (preference.genre) ? 2 : 1;
-	if(strcmp(chosen_song->data.genre[1], pHead->data.genre[1])) score += (preference.genre) ? 2 : 1;
+	if(strcmp(chosen_song->data.genre[0], pHead->data.genre[0])) score += (preference.genre) ? 1 : 0;
+	if(strcmp(chosen_song->data.genre[1], pHead->data.genre[1])) score += (preference.genre) ? 1 : 0;
 
-	if (preference.bpm == FAST && chosen_song->data.bpm > 140) score += 2;
-	if (preference.bpm == MEDIUM && chosen_song->data.bpm < 140 && chosen_song->data.bpm > 100) score += 2;
-	if (preference.bpm == SLOW && chosen_song->data.bpm < 100) score += 2;
+	if (preference.bpm == FAST && chosen_song->data.bpm > 140) score += 1;
+	if (preference.bpm == MEDIUM && chosen_song->data.bpm < 140 && chosen_song->data.bpm > 100) score += 1;
+	if (preference.bpm == SLOW && chosen_song->data.bpm < 100) score += 1;
 
-	if (pHead->data.length.minutes >= chosen_song->data.length.minutes-1 && pHead->data.length.minutes <= chosen_song->data.length.minutes+1) score += 2;
+	if (pHead->data.length.minutes >= chosen_song->data.length.minutes-1 && pHead->data.length.minutes <= chosen_song->data.length.minutes+1) score += 1;
 
-	if (pHead->data.amount_of_words >= chosen_song->data.amount_of_words-100 && pHead->data.amount_of_words <= chosen_song->data.amount_of_words+100) score += 2;
+	if (pHead->data.amount_of_words >= chosen_song->data.amount_of_words-100 && pHead->data.amount_of_words <= chosen_song->data.amount_of_words+100) score += 1;
 
-	if (pHead->data.is_happy == preference.is_happy) score += 2;
-	if(strcmp(pHead->data.language, chosen_song->data.language) == 0 && preference.language) score += 2;
-	if(pHead->data.is_major == chosen_song->data.is_major) score += 2;
+	if (pHead->data.is_happy == preference.is_happy) score += 1;
+	if(strcmp(pHead->data.language, chosen_song->data.language) == 0 && preference.language) score += 1;
+	if(pHead->data.is_major == chosen_song->data.is_major) score += 1;
 
 	pHead->data.similarity_score = score;
 }
