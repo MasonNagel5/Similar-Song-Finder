ddb0495f9edef63da5b426e8a81d21e57b6d4fad
Logan Meyers <theterrarian03@gmail.com>
2025-02-15 17:46:22 -0800

    Added similarity scorer

--- Similar-Song-Finder/logan.c
+++ Similar-Song-Finder/logan.c
@@ -12,5 +12,35 @@ void traverse_through_available(Node** pHead) {
 		pCur = pCur->pNext;
 	}
 
-	
+	pCur = *pHead;
+
+	while (pCur != NULL) {
+		// score songs here
+		// score_record(pCur, chosen_song)
+
+		pCur = pCur->pNext;
+	}
+}
+
+void score_record(Node *pHead, Node *chosen_song, Preferences preference) {
+	int score = 0;
+
+	if (strcmp(chosen_song->data.artist, pHead->data.artist) == 0) score += (preference.artist) ? 2 : 1;
+
+	if(strcmp(chosen_song->data.genre[0], pHead->data.genre[0])) score += (preference.genre) ? 2 : 1;
+	if(strcmp(chosen_song->data.genre[1], pHead->data.genre[1])) score += (preference.genre) ? 2 : 1;
+
+	if (preference.bpm == FAST && chosen_song->data.bpm > 140) score += 2;
+	if (preference.bpm == MEDIUM && chosen_song->data.bpm < 140 && chosen_song->data.bpm > 100) score += 2;
+	if (preference.bpm == SLOW && chosen_song->data.bpm < 100) score += 2;
+
+	if (pHead->data.length.minutes >= chosen_song->data.length.minutes-1 && pHead->data.length.minutes <= chosen_song->data.length.minutes+1) score += 2;
+
+	if (pHead->data.amount_of_words >= chosen_song->data.amount_of_words-100 && pHead->data.amount_of_words <= chosen_song->data.amount_of_words+100) score += 2;
+
+	if (pHead->data.is_happy == preference.is_happy) score += 2;
+	if(strcmp(pHead->data.language, chosen_song->data.language) == 0 && preference.language) score += 2;
+	if(pHead->data.is_major == chosen_song->data.is_major) score += 2;
+
+	pHead->data.similarity_score = score;
 }
--- Similar-Song-Finder/similar_song_finder.h
+++ Similar-Song-Finder/similar_song_finder.h
@@ -5,9 +5,24 @@
 #include <stdlib.h>
 #include <string.h>
 #include "list.h"
+
+typedef enum bpm_range {
+    FAST,  // >140
+    MEDIUM,  // 100-140
+    SLOW  // <100
+} BPM_RANGE;
+
+typedef struct preferences {
+    int artist;  // 1 for prefers same artist, 0 otherwise
+    int genre;  // 1 for prefers same genre, 0 otherwise
+    BPM_RANGE bpm;  // etc
+    int is_happy;  // etc
+    int language;
+} Preferences;
+
 Node* getinitialsong(Node* pHead);
 void traverse_through_available(Node** pHead);
-
+void score_record(Node *pHead, Node *chosen_song, Preferences priority);
 
 
 #endif
